5675b0e58964500d3d6f88a3a537e18d25e57d80
Vivek Sorathiya <vivek.sorathiya@kevit.io>
2022-12-20 18:44:37 +0530

    feat: add wallpaper setter

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 const puppeteer = require("puppeteer");
-const chalk = require("chalk");
+const path = require("path");
 const axios = require("axios");
 const dayjs = require("dayjs");
 const figlet = require("figlet");
+const cron = require("node-cron");
+const wallpaper = require("wallpaper");
 
 const config = {
   email: "keka email",
@@ -59,7 +61,7 @@ async function getData(token) {
 }
 
 function calculateDuration(data) {
-  const today = data?.reverse()[0];
+  const today = data.find(timer => new Date(timer?.attendanceDate).toISOString() === new Date(dayjs().format('YYYY-MM-DD')).toISOString())
   const validInOutPairs = today?.validInOutPairs;
   const current = dayjs();
   let workedTime = 0;
@@ -85,36 +87,74 @@ function calculateDuration(data) {
   return workedTime;
 }
 
-(async () => {
+let time, data;
+
+function setTime() {
+  let total = 8.5;
+  const workedTime = calculateDuration(data);
+  if (workedTime) total -= workedTime;
+  let decimal = total - Math.floor(total)
+  let abs = Math.floor(total)
+  let min = decimal * 60
+
+  time = abs + "h" + " " + (Math.floor(min)).toFixed(0) + "m " + ((min - Math.floor(min)) * 60).toFixed(0) + "s"
+  return total
+}
+
+async function main() {
   const authToken = await getAuthToken();
-  setInterval(async () => {
-    const data = await getData(authToken);
-    const workedTime = calculateDuration(data);
-    let total = 8.5;
-    if (workedTime) total -= workedTime;
-    process.stdout.moveCursor(0, -1); // up one line
-    process.stdout.clearLine(1);
-    process.stdout.moveCursor(0, -1); // up one line
-    process.stdout.clearLine(1);
-    process.stdout.moveCursor(0, -1); // up one line
-    process.stdout.clearLine(1);
-    process.stdout.moveCursor(0, -1); // up one line
-    process.stdout.clearLine(1);
-    process.stdout.moveCursor(0, -1); // up one line
-    process.stdout.clearLine(1);
-    process.stdout.cursorTo(0);
-    process.stdout.write(
-      chalk[getColor(total)](
-        figlet.textSync(
-          total.toFixed(2) + " h" + "  " + (total * 60).toFixed(2) + " m"
-        )
-      )
-    );
-    if (total <= 0) {
+  data = await getData(authToken);
+
+  setInterval((async () => {
+    const hrs = setTime()
+    // process.stdout.moveCursor(0, -1); // up one line
+    // process.stdout.clearLine(1);
+    // process.stdout.moveCursor(0, -1); // up one line
+    // process.stdout.clearLine(1);
+    // process.stdout.moveCursor(0, -1); // up one line
+    // process.stdout.clearLine(1);
+    // process.stdout.moveCursor(0, -1); // up one line
+    // process.stdout.clearLine(1);
+    // process.stdout.moveCursor(0, -1); // up one line
+    // process.stdout.clearLine(1);
+    // process.stdout.moveCursor(0, -1); // up one line
+    // process.stdout.clearLine(1);
+    // process.stdout.cursorTo(0);
+    // process.stdout.write(
+    //     figlet.textSync(
+    //       str
+    //   )
+    // );
+
+    if (hrs <= 0) {
       //handle timeout case here
     }
-  }, 5000);
-})();
+  }), 1000);
+
+  const browser = await puppeteer.launch({
+    headless: true,
+      ignoreHTTPSErrors: true,
+      args: [`--window-size=1920,1080`],
+      defaultViewport: {
+        width:1920,
+        height:1080
+      }
+  })
+  const page = await browser.newPage()
+  await page.goto(path.join(__dirname, './index.html'))
+  setInterval(async () => {
+    if (time?.trim() !== '8h 30m 0s') {
+      await page.evaluate((time) => document.querySelector('.text').innerHTML = time, time);
+      await setWallpaper(page)
+    }
+  }, 500)
+
+
+  cron.schedule('15 15 * * *', async () => {
+    const authToken = await getAuthToken()
+    data = await getData(authToken)
+  })
+}
 
 async function sleep(milliseconds) {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
@@ -126,3 +166,12 @@ function getColor(remaining) {
   else if (remaining > 1) return "yellow";
   else return "green";
 }
+
+async function setWallpaper(page) {
+  await page.screenshot({
+    path: 'ss.png'
+  })
+  await wallpaper.set('./ss.png')
+}
+
+main().then().catch(e => console.log(e))
